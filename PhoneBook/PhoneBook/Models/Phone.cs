56cd83da8d8d83400cbb6612ef13e62d1424d9fd
kristiqndinev <kristiqndinev@abv.bg>
2016-05-25 02:51:54 +0300

    2

---
+++ PhoneBook/PhoneBook/Models/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+
+        public string Number { get; set; }
+
+        public int ContactId { get; set; }
+
+        public virtual Contact Contact { get; set; }
+    }
+}
